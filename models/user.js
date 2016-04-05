d5cca6ffdfbadc88b5a5ae13c44d29b87d218f70
Paige Bolduc <paigebolduc@gmail.com>
2016-04-05 12:17:47 -0400

    eliminate unnecessary dependencies in user model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,5 @@
 'use strict';
 
-var mongoose = require('mongoose');
-var jwt = require('jwt-simple');
-var JWT_SECRET = process.env.JWT_SECRET;
-
 var userSchema = new mongoose.Schema({
   uid: String,
   username: {type: String, required: true},
