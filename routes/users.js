a54f7ddc144f1bd465ca8096f2aa759d453ab52c
Paige Bolduc <paigebolduc@gmail.com>
2016-04-05 14:39:40 -0400

    add module.js file for angular app structure

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var authMiddleware = require('../config/auth');
+var User = require('../models/user');
+var userMethods = require("../modules/user");
+
+var Firebase = require('firebase');
+var ref = new Firebase('https://meandates.firebaseio.com/');
+
+
+router.post('/register', userMethods.register, function(req, res, next) {
+  res.send("register!");
+});
+
+router.post('/login', userMethods.login, function(req, res, next) {
+  res.send("Logged In");
+});
+
+router.post("/logout", function(req, res, next) {
+  res.clearCookie("userToken").redirect("/"); 
 });
 
 module.exports = router;
---
+++ src/js/module.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var app = angular.module("bookMarkers", ["ui.router"]);
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state("home", {url: "/", templateUrl: "/partials/home.html", controller: "homeCtrl"})
+    .state("register", {url: "/register", templateUrl: "/partials/register.html", controller: "registerCtrl"})
+    .state("login", {url: "/login", templateUrl: "/partials/login.html", controller: "loginCtrl"})
+    .state("dash", {url: "/dashboard", templateUrl: "/partials/dashboard.html", controller: "dashCtrl"})
+    .state("profile", {url: "/profile", templateUrl: "/partials/profile.html", controller: "profileCtrl"})
+    .state("editProfile", {url: "/profile/edit", templateUrl: "/partials/editProfile.html", controller: "editProfileCtrl"})
+    .state("search", {url: "/profile/search", templateUrl: "/partials/search.html", controller: "searchCtrl"})
+
+  $urlRouterProvider.otherwise("/");
+});
