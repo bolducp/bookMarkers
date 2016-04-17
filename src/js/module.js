66c4c801443c6d8366b99aab756cee1ccd7134d9
Paige Bolduc <paigebolduc@gmail.com>
2016-04-17 12:46:54 -0400

    Improve formatting for  routing

--- src/js/module.js
+++ src/js/module.js
@@ -4,12 +4,36 @@ var app = angular.module("bookMarkers", ["ui.router"]);
 
 app.config(function($stateProvider, $urlRouterProvider){
   $stateProvider
-    .state("home", {url: "/", templateUrl: "/partials/home.html", controller: "homeCtrl"})
-    .state("register", {url: "/register", templateUrl: "/partials/register.html", controller: "registerCtrl"})
-    .state("login", {url: "/login", templateUrl: "/partials/login.html", controller: "loginCtrl"})
-    .state("dash", {url: "/dashboard", templateUrl: "/partials/dashboard.html", controller: "dashCtrl"})
-    .state("profile", {url: "/profile", templateUrl: "/partials/profile.html", controller: "profileCtrl"})
-    .state("editProfile", {url: "/profile/edit", templateUrl: "/partials/editProfile.html", controller: "editProfileCtrl"})
-    
+    .state("home", {
+      url: "/",
+      templateUrl: "/partials/home.html",
+      controller: "homeCtrl"
+    })
+    .state("register", {
+      url: "/register",
+      templateUrl: "/partials/register.html",
+      controller: "registerCtrl"
+    })
+    .state("login", {
+      url: "/login",
+      templateUrl: "/partials/login.html",
+      controller: "loginCtrl"
+    })
+    .state("dash", {
+      url: "/dashboard",
+      templateUrl: "/partials/dashboard.html",
+      controller: "dashCtrl"
+    })
+    .state("profile", {
+      url: "/profile",
+      templateUrl: "/partials/profile.html",
+      controller: "profileCtrl"
+    })
+    .state("editProfile", {
+      url: "/profile/edit",
+      templateUrl: "/partials/editProfile.html",
+      controller: "editProfileCtrl"
+    })
+
   $urlRouterProvider.otherwise("/");
 });
